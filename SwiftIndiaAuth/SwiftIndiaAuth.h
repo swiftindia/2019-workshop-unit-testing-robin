ed6db30d99661e6b2a8b6c4c1abb54c6f8e026a6
Robin Malhotra <me@rmalhotra.com>
2019-07-22 01:17:57 +0530

    Initial Commit

---
+++ SwiftIndiaAuth/SwiftIndiaAuth.h
@@ -0,0 +1,19 @@
+//
+//  SwiftIndiaAuth.h
+//  SwiftIndiaAuth
+//
+//  Created by Robin Malhotra on 22/07/19.
+//  Copyright © 2019 rmalhotra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftIndiaAuth.
+FOUNDATION_EXPORT double SwiftIndiaAuthVersionNumber;
+
+//! Project version string for SwiftIndiaAuth.
+FOUNDATION_EXPORT const unsigned char SwiftIndiaAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftIndiaAuth/PublicHeader.h>
+
+
